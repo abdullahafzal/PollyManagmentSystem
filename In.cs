6ad85acb0b636eea16778c66e56ffa0a2bbbbe04
abdullahafzal <48883517+abdullahafzal@users.noreply.github.com>
2019-05-22 17:39:51 +0500

    InClass
    Parsing Exception Not Handled

--- In.cs
+++ In.cs
@@ -15,6 +15,10 @@ namespace PMS
 {
     public partial class In : Form
     {
+        AutoCompleteStringCollection companyCollection = new AutoCompleteStringCollection();
+        AutoCompleteStringCollection itemCollection = new AutoCompleteStringCollection();
+
+
         public In()
         {
             InitializeComponent();
@@ -32,13 +36,12 @@ namespace PMS
 
 
             SqlDataReader dr = cmd.ExecuteReader();
-            AutoCompleteStringCollection mycollection = new AutoCompleteStringCollection();
             while (dr.Read())
             {
-                mycollection.Add(dr.GetString(0));
+                companyCollection.Add(dr.GetString(0));
 
             }
-            textBoxCompany.AutoCompleteCustomSource = mycollection;
+            textBoxCompany.AutoCompleteCustomSource = companyCollection;
             conn.Close();
         }
         private void LoadItemList()
@@ -51,13 +54,12 @@ namespace PMS
 
 
             SqlDataReader dr = cmd.ExecuteReader();
-            AutoCompleteStringCollection mycollection = new AutoCompleteStringCollection();
             while (dr.Read())
             {
-                mycollection.Add(dr.GetString(0));
+                itemCollection.Add(dr.GetString(0));
 
             }
-            textBoxItem.AutoCompleteCustomSource = mycollection;
+            textBoxItem.AutoCompleteCustomSource = itemCollection;
             conn.Close();
         }
 
@@ -114,21 +116,31 @@ namespace PMS
         }
         string InvIDtemp = "";
         int rowindex;
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+
             rowindex = e.RowIndex;
+            try
+            {
+                inObj.Inv_ID = int.Parse(dataGridView1.Rows[rowindex].Cells[0].Value.ToString());
+                InvIDtemp = inObj.Inv_ID.ToString();
+                dtpEnteryTime.Text = dataGridView1.Rows[rowindex].Cells[1].Value.ToString();
+                inObj.EnteryTime = dtpEnteryTime.Value.ToLocalTime();
 
-            inObj.Inv_ID =int.Parse(dataGridView1.Rows[rowindex].Cells[0].Value.ToString());
-            InvIDtemp=inObj.Inv_ID.ToString();
-            dtpEnteryTime.Text = dataGridView1.Rows[rowindex].Cells[1].Value.ToString();
-            inObj.EnteryTime = dtpEnteryTime.Value.ToLocalTime();
+                inObj.Sup_Company = dataGridView1.Rows[rowindex].Cells[2].Value.ToString();
+                inObj.Item_Name = dataGridView1.Rows[rowindex].Cells[3].Value.ToString();
+                if (IsAllDigits(dataGridView1.Rows[rowindex].Cells[4].Value.ToString())) {
+                    inObj.Quantity = int.Parse(dataGridView1.Rows[rowindex].Cells[4].Value.ToString());
+                }
 
-            inObj.Sup_Company = dataGridView1.Rows[rowindex].Cells[2].Value.ToString();
-            inObj.Item_Name = dataGridView1.Rows[rowindex].Cells[3].Value.ToString(); ;
-            inObj.Quantity =int.Parse (dataGridView1.Rows[rowindex].Cells[4].Value.ToString()) ;
-            inObj.T_Price =int.Parse(dataGridView1.Rows[rowindex].Cells[5].Value.ToString()) ;
-            inObj.Comments = dataGridView1.Rows[rowindex].Cells[6].Value.ToString();
+                inObj.T_Price = int.Parse(dataGridView1.Rows[rowindex].Cells[5].Value.ToString());
 
+                inObj.Comments = dataGridView1.Rows[rowindex].Cells[6].Value.ToString();
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show("Values are incorrect"+ ex);
+            }
         }
         bool IsAllDigits(string s)
         {
@@ -139,19 +151,36 @@ namespace PMS
             }
             return true;
         }
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
+        private Boolean btnAddEntriesChecks() {
             if (String.IsNullOrEmpty(textBoxCompany.Text) || String.IsNullOrEmpty(textBoxItem.Text) ||
-                String.IsNullOrEmpty(textBoxQuantity.Text) || String.IsNullOrEmpty(textBoxPrice.Text))
+        String.IsNullOrEmpty(textBoxQuantity.Text) || String.IsNullOrEmpty(textBoxPrice.Text))
             {
                 MessageBox.Show("Insertion Failed, Please Enter all the values");
-
+                return false;
             }
             else if (!IsAllDigits(textBoxQuantity.Text) || !IsAllDigits(textBoxPrice.Text))
+            {
                 MessageBox.Show("Quantity & Price Should only have Numeric Values  ");
+                return false;
+            }
 
+            else if (!companyCollection.Contains(textBoxCompany.Text))
+            {
+                MessageBox.Show("Please Enter valid Company Name");
+                return false;
+            }
 
-            else
+            else if (!itemCollection.Contains(textBoxItem.Text))
+            {
+                MessageBox.Show("Please Enter valid Item Name");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (btnAddEntriesChecks())
             {
                 inObj.EnteryTime = dtpEnteryTime.Value.ToLocalTime();
                 inObj.Sup_Company = textBoxCompany.Text;
@@ -176,27 +205,55 @@ namespace PMS
                 }
             }
         }
+        private Boolean btnUpdateEntriesChecks()
+        {
+ 
+                if (String.IsNullOrEmpty(inObj.Sup_Company) || String.IsNullOrEmpty(inObj.Item_Name))
+                {
+                    MessageBox.Show("Updation Failed because of empty fields");
+                    return false;
+                }
 
+                else if (!companyCollection.Contains(inObj.Sup_Company))
+                {
+                    MessageBox.Show("Please Update a valid Company Name");
+                    return false;
+                }
+
+                else if (!itemCollection.Contains(inObj.Item_Name))
+                {
+                    MessageBox.Show("Please Update a valid Item Name");
+                    return false;
+                }
+                return true;
+
+            }
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Update ");
 
             var args = new DataGridViewCellEventArgs(0, rowindex);
-            dataGridView1_CellContentClick(dataGridView2, args);
+
+            DataGridView1_CellClick(dataGridView2, args);
+
 
             bool success = inObj.Update(inObj);
-            if (success == true)
-            {
-                MessageBox.Show("Updated Successfully");
+                if (success == true)
+                {
+                    MessageBox.Show("Updated Successfully");
 
-                //Show Data
-                DataTable dt = inObj.Select();
-                dataGridView1.DataSource = dt;
+                    //Show Data
+                    DataTable dt1 = inObj.Select();
 
-            }
-            else
-            {
-                MessageBox.Show("Updation Failed");
-            }
+                    dataGridView1.DataSource = dt1;
+
+                }
+                else
+                {
+                    MessageBox.Show("Updation Failed");
+               }
+            
+
         }
 
         private void btnShow_Click(object sender, EventArgs e)
@@ -228,8 +285,20 @@ namespace PMS
             }
         }
 
+        private void DtpEnteryTime_ValueChanged(object sender, EventArgs e)
+        {
 
+        }
 
+        private void TextBoxItem_TextChanged(object sender, EventArgs e)
+        {
 
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+              
+            }
     }
 }
